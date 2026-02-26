c0a3401d3761fecd49fd3b5061f98443c23a82a1
water <wateruby8@gmail.com>
2026-02-26 11:33:26 +0800

    實作RESTful API 串接 : 登入與取得產品資料

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,218 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [isAuth, setIsAuth] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [tempProduct, setTempProduct] = useState(null);
+
+  // 表單資料內容處理， 更新setFormData
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  // 表單提交，向 API 發送登入請求並處理登入結果
+  const handleSubmit = async (e) => {
+    // 停用預設事件
+    e.preventDefault();
+
+    try {
+      // 做連線
+      const response = await axios.post(`${API_BASE}/admin/signin`, formData);
+      // 設定 cookie
+      const { token, expired } = response.data;
+      document.cookie = `loginToken=${token};expires=${new Date(expired)};`;
+      axios.defaults.headers.common.Authorization = token;
+      // 登入成功
+      setIsAuth(true);
+      // 取得產品資料
+      getProducts();
+    } catch (error) {
+      // 登入失敗
+      setIsAuth(false);
+      console.log("登入失敗：", error.response?.data);
+    }
+  };
+
+  // 確認是否登入 驗證 cookie 的 token
+  const checkLogin = async () => {
+    try {
+      const token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("loginToken="))
+        ?.split("=")[1];
+      if (token) {
+        axios.defaults.headers.common.Authorization = token;
+        const res = await axios.post(`${API_BASE}/api/user/check`);
+        console.log("Token 驗證成功：", res.data);
+      }
+    } catch (error) {
+      console.log("Token 驗證失敗：", error.response?.data);
+    }
+  };
+
+  // 獲取產品列表資料
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(
+        `${API_BASE}/api/${API_PATH}/admin/products`,
+      );
+      setProducts(response.data.products);
+    } catch (error) {
+      console.log("取得資料失敗：", error.response?.data);
+    }
+  };
 
   return (
     <>
-     
+      {isAuth ? (
+        <div className="container">
+          <button
+            className="btn btn-danger my-5"
+            type="button"
+            onClick={checkLogin}
+          >
+            確認是否登入
+          </button>
+          <div className="row mt-5">
+            <div className="col-md-6">
+              <h2>產品列表</h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>產品名稱</th>
+                    <th>原價</th>
+                    <th>售價</th>
+                    <th>是否啟用</th>
+                    <th>查看細節</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products && products.length > 0 ? (
+                    products.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.title}</td>
+                        <td>{item.origin_price}</td>
+                        <td>{item.price}</td>
+                        <td>{item.is_enabled ? "啟用" : "未啟用"}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => setTempProduct(item)}
+                          >
+                            查看細節
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="5">尚無產品資料</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <div className="col-md-6">
+              <h2>單一產品細節</h2>
+              {tempProduct ? (
+                <div className="card mb-3">
+                  <img
+                    src={tempProduct.imageUrl}
+                    className="card-img-top primary-image"
+                    alt="主圖"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {tempProduct.title}
+                      <span className="badge bg-primary ms-2">
+                        {tempProduct.category}
+                      </span>
+                    </h5>
+                    <p className="card-text">
+                      商品描述：{tempProduct.description}
+                    </p>
+                    <p className="card-text">商品內容：{tempProduct.content}</p>
+                    <div className="d-flex">
+                      <p className="card-text text-secondary">
+                        <del>{tempProduct.origin_price}</del>
+                      </p>
+                      元 / {tempProduct.price} 元
+                    </div>
+                    <h5 className="mt-3">更多圖片：</h5>
+                    <div className="d-flex flex-wrap">
+                      {tempProduct.imagesUrl?.map((url, index) => (
+                        <img
+                          key={index}
+                          src={url}
+                          className="images"
+                          alt="副圖"
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <p className="text-secondary">請選擇一個商品查看</p>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="container login">
+          <div className="row justify-content-center">
+            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div className="col-8">
+              <form id="form" className="form-signin" onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="username"
+                    placeholder="name@example.com"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    required
+                    autoFocus
+                  />
+                  <label htmlFor="username">Email address</label>
+                </div>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <button
+                  className="btn btn-lg btn-primary w-100 mt-3"
+                  type="submit"
+                >
+                  登入
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
